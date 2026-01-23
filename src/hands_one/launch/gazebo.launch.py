b09c6e7a6b599be0230bf5fcea2d3546010bb5e8
sainathrakesh <nsainathrakesh@gmail.com>
2026-01-23 23:52:46 +0100

    Scaled URDF to correct sizing of the robotic hand

--- src/hands_one/launch/gazebo.launch.py
+++ src/hands_one/launch/gazebo.launch.py
@@ -1,16 +1,18 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
     pkg_path = get_package_share_directory('hands_one')
-    urdf_file = os.path.join(pkg_path, 'urdf', 'hands_one.urdf')
+    urdf_file = os.path.join(pkg_path, 'urdf', 'sample.urdf')
+
+    gazebo_pkg = get_package_share_directory('gazebo_ros')
+    world_file = os.path.join(pkg_path, 'worlds', 'empty.world') 
 
     return LaunchDescription([
-
-        # Robot State Publisher
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -19,20 +21,25 @@ def generate_launch_description():
             }]
         ),
 
-        # Gazebo
-        ExecuteProcess(
-            cmd=['gazebo', '--verbose'],
-            output='screen'
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(gazebo_pkg, 'launch', 'gazebo.launch.py')
+            ),
+            launch_arguments={'world': world_file}.items()
         ),
 
-        # Spawn robot into Gazebo
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            arguments=[
-                '-topic', 'robot_description',
-                '-entity', 'hands_one'
-            ],
-            output='screen'
+        TimerAction(
+            period=5.0,
+            actions=[
+                Node(
+                    package='gazebo_ros',
+                    executable='spawn_entity.py',
+                    arguments=[
+                        '-topic', 'robot_description',
+                        '-entity', 'hands_one'
+                    ],
+                    output='screen'
+                )
+            ]
         ),
     ])
